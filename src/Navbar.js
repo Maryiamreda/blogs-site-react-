10f91d775ddc4f8df8dc97eca78389fee15ba66a
Maryiamreda <maryiamreda@gmail.com>
2024-08-19 01:28:56 +0300

    useEffect Cleanup

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Net Ninja Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px",
                     borderStyle: "none"
-                }}>New Blog</a>
+                }}>New Blog</Link>
 
             </div>
 
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,30 +1,50 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
     const [data, setData] = useState(null
+
         //     [
         //     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         //     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         //     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
         // ]
-    );
 
+    );
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url).then(
-                res => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    if (!res.ok) { // error coming back from server
+                        throw Error('could not fetch the data for that resource');
+                    }
                     return res.json();
-                }
-            ).then(data => {
-                setData(data);
-                setIsPending(false);
-            })
-        }, 1000)
+                })
+                .then(data => {
+                    setIsPending(false);
+                    setData(data);
+                    setError(null);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted')
+                    } else {
+                        // auto catches network / connection error
+                        setIsPending(false);
+                        setError(err.message);
+                    }
+                })
+        }, 1000);
 
+        // abort the fetch
+        return () => abortCont.abort();
+    }, [url])
 
-    }, [url]);//whenever the url changes it will render to get this data
-    return { data, isPending }
+    return { data, isPending, error };
 }
+
 export default useFetch;
