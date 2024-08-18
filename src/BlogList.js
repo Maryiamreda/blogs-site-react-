b174fb10a4a46949b01474076ee0cf8301d902cd
Maryiamreda <maryiamreda@gmail.com>
2024-08-19 02:28:33 +0300

    Reusing Custom Hooks

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({ blogs, title, handleDelete }) => {
     return (
         <div className="blog-list">
@@ -8,8 +10,11 @@ const BlogList = ({ blogs, title, handleDelete }) => {
             {blogs.map((blog) => (
 
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>written by {blog.author}</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>written by {blog.author}</p>
+                    </Link>
+
                     {/* <button onClick={() => handleDelete(blog.id)}>delete blog</button> */}
                 </div>
             ))} </div>
